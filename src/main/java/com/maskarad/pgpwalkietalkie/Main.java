65bf961247681538bdea01f1adefec8456277e1b
maskaradd1 <rustam.ahmadzada@outlook.com>
2024-10-25 00:01:31 +0400

    created interface

--- src/main/java/com/maskarad/pgpwalkietalkie/Main.java
+++ src/main/java/com/maskarad/pgpwalkietalkie/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("A!");
+        Scene scene = new Scene(fxmlLoader.load(), 250, 100);
+        stage.setTitle("PGP Walkie-Talkie");
         stage.setScene(scene);
         stage.show();
     }
